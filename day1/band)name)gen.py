f278e02f623c2295bbfb5612fd8e4e67b89885b6
Diya-siri <106438761+Diya-siri@users.noreply.github.com>
2022-12-23 23:53:41 +0530

    day 1

---
+++ day1/band)name)gen.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator program\n")
+n=input("whats the name of the city u grew up in?\n")
+m=input("whats your pets name?\n")
+print("your band name could be "  + n + " " + m)
